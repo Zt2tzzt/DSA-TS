895f5196f12378c633cfad70182270b53ea6acf7
ZeT1an <584633819@qq.com>
2023-07-08 13:53:46 +0800

    fix: v9 03-队列结构;

--- demo-project/02-队列/03-面试题-约瑟夫环.ts
+++ demo-project/02-队列/03-面试题-约瑟夫环.ts
@@ -3,19 +3,19 @@ import { ArrayQueue } from './01-ArrayQueue';
 /**
  * @description: 此函数用于：解决与瑟夫环问题。
  * @Author: ZeT1an
- * @param {number} members 所有成员的编号总数，最小值为 1。
- * @param {number} num 间隔的个数。
+ * @param {number} n 所有成员的编号总数，最小值为 1。
+ * @param {number} m 间隔的个数。
  * @return {number} 或者者的编号。
  */
-function josephusProblem<T>(members: number, num: number): number {
+function josephusProblem<T>(n: number, m: number): number {
   const queue = new ArrayQueue<number>();
 
-  for (let i = 1; i <= members; i++) {
+  for (let i = 0; i < n; i++) {
     queue.enqueue(i);
   }
 
   while (queue.size() > 1) {
-    for (let i = 0; i < num; i++) {
+    for (let i = 1; i < m; i++) {
       queue.enqueue(queue.dequeue()!);
     }
     const index = queue.dequeue();
