b9cf37e99f4f1a2908f90146e1e273d361384898
ZeT1an <zetian.zhu@foxmail.com>
2023-08-16 14:19:20 +0800

    fix: v75;

--- demo-project/00_测试代码/01-对象之间的比较方式.ts
+++ demo-project/00_测试代码/01-对象之间的比较方式.ts
@@ -14,3 +14,17 @@ console.log(p1 > p2); // true
 const p3 = new Person("zzt", 26)
 console.log(p3 === p2) // false
 console.log(p3 == p2) // false
+
+class CustomNumber {
+  constructor(private value: number) {}
+
+  equals(other: CustomNumber): boolean {
+    return this.value === other.value;
+  }
+}
+
+const num1 = new CustomNumber(5);
+const num2 = new CustomNumber(5);
+
+console.log(num1.equals(num2));  // 输出: true
+
