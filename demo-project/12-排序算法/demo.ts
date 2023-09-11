a2a8ae98ff76138863ec102561b9905e223c4bfd
ZeT1an <zetian.zhu@foxmail.com>
2023-09-11 16:06:13 +0800

    fix: v138;

--- demo-project/12-排序算法/demo.ts
+++ demo-project/12-排序算法/demo.ts
@@ -1,70 +1,44 @@
-// import { testSort } from 'hy-algokit';
+import { testSort } from 'hy-algokit';
 
 function swap(arr: number[], i: number, j: number) {
   ;[arr[i], arr[j]] = [arr[j], arr[i]]
 }
 
-function isSorted(arr: number[]) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[i + 1]) return false
+// -----------------------
+
+function heapSort(arr: number[]): number[] {
+  const n = arr.length
+
+  // 原地建堆
+  const lastNoneLeafNode = Math.floor((n - 1) / 2 - 1)
+  for (let i = lastNoneLeafNode; i >= 0; i--) {
+    heapfy_down(arr, n, i)
   }
 
-  return true
-}
-
-type SortFn = (arr: number[]) => number[]
-
-function testSort(fn: SortFn) {
-  const arr = Array.from({ length: 10 }, () => Math.floor(Math.random() * 100))
-  console.log('排序前的数组：', arr)
-
-  const newArr = fn(arr)
-  console.log('排序后的数组：', newArr)
-  console.log('排序后的数组是否有序：', isSorted(newArr))
-}
-
-// ------------
-
-function quickSort(arr: number[]): number[] {
-  partition(0, arr.length - 1)
-
-  function partition(start: number, end: number) {
-    if (start >= end) return
-
-    // 三叔取中
-    const mid = Math.floor((start + end) / 2)
-    if (start < mid && mid < end) {
-      if (arr[start] > arr[mid]) swap(arr, start, mid)
-      if (arr[mid] > arr[end]) swap(arr, mid, end)
-      if (arr[start] > arr[mid]) swap(arr, start, mid)
-      swap(arr, mid, end)
-    }
-
-    const pivot = arr[end]
-
-    let i = 0, j = end - 1
-    while (i <= j) {
-      while (arr[i] < pivot) {
-        i++
-      }
-      while (arr[j] > pivot) {
-        j--        
-      }
-
-      if (i <= j) {
-        swap(arr, i, j)
-        i++
-        j--
-      }
-    }
-
-    swap(arr, i, end)
-    partition(0, j)
-    partition(i + 1, end)
+  // 排序操作
+  for (let i = n - 1; i > 0; i--) {
+    swap(arr, 0, i)
+    heapfy_down(arr, i, 0)
   }
 
   return arr
 }
 
-testSort(quickSort)
+function heapfy_down(arr: number[], n: number, index: number) {
+  while (2 * index + 1 < n) {
+    const leftChildIndex = 2 * index + 1
+    const rightchildIndex = leftChildIndex + 1
+
+    let largeIndex = leftChildIndex
+    if (rightchildIndex < n && arr[rightchildIndex] > arr[leftChildIndex]) {
+      largeIndex = rightchildIndex
+    }
+
+    if (arr[index] > arr[largeIndex]) break
+    swap(arr, index, largeIndex)
+    index = largeIndex
+  }
+}
+
+testSort(heapSort)
 // measureSort(selectionSort)
