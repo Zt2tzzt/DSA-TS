42657799e40af8ce452cba104539d6d64a1df43f
ZeT1an <zetian.zhu@foxmail.com>
2023-09-18 17:50:31 +0800

    fix: v147;

--- demo-project/12-排序算法/demo.ts
+++ demo-project/12-排序算法/demo.ts
@@ -6,22 +6,35 @@ function swap(arr: number[], i: number, j: number) {
 
 // -----------------------
 
-function selectionSort(arr: number[]): number[] {
-  const n = arr.length
+function heapSort(arr: number[]): number[] {
+  const n =arr.length
 
-  
-  for (let i = 0; i < n - 1; i++) {
-    let minIndex = i
+  const lastNoneLeafNodeIndex = Math.floor((n - 1) / 2 - 1)
+  for (let i = lastNoneLeafNodeIndex; i >= 0; i--) {
+    heapfy_down(arr, n, i)
+  }
 
-    for (let j = i + 1; j < n; j++) {
-      if (arr[j] < arr[minIndex]) minIndex = j
-    }
-
-    if (i !== minIndex) swap(arr, i, minIndex)
+  for (let i = n - 1; i > 0; i--) {
+    swap(arr, 0, i)
+    heapfy_down(arr, i, 0)
   }
 
   return arr
 }
 
-testSort(selectionSort)
+function heapfy_down(arr: number[], n: number, index: number) {
+  while (2 * index + 1 < n) {
+    const leftChildIndex = 2 * index + 1
+    const rightChildIndex = leftChildIndex + 1
+
+    const largeIndex = rightChildIndex < n && arr[rightChildIndex] > arr[leftChildIndex] ? rightChildIndex : leftChildIndex
+
+    if (arr[index] >= arr[largeIndex]) break
+
+    swap(arr, index, largeIndex)
+    index = largeIndex
+  }
+}
+
+testSort(heapSort)
 // measureSort(selectionSort)
